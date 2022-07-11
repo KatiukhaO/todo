1ddc6f2414f156916036adb960d864a102f0747c
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-07-11 17:14:04 +0300

    I google widget for datatime

--- todo/forms.py
+++ todo/forms.py
@@ -15,7 +15,10 @@ class TaskForm(forms.ModelForm):
         widget=forms.CheckboxSelectMultiple,
         required=False
     )
-    deadline = forms.SplitDateTimeField()
+    deadline = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        required=False
+    )
 
     class Meta:
         model = Task
